94d3f88636bd986cddd9fe58f874a1892983fc5e
wongyujing <wongyujing@163.com>
2020-05-14 19:02:08 +0800

    delete useless variable

--- router/index.js
+++ router/index.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
-const path = require('path');
 const Router = require('koa-router');
-const ejs = require('ejs');
-const MarkdownIt = require('markdown-it');
 const article = require('../service/article');
 
 const router = new Router();
