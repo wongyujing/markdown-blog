4e68aab446cab7e36b3f486f743c9f00ba1e2cb5
wongyujing <wongyujing@163.com>
2020-05-14 19:01:40 +0800

    add service of article

--- router/index.js
+++ router/index.js
@@ -3,39 +3,17 @@ const path = require('path');
 const Router = require('koa-router');
 const ejs = require('ejs');
 const MarkdownIt = require('markdown-it');
+const article = require('../service/article');
 
 const router = new Router();
 
 router.get('/', ctx => {
-  const html = fs.readFileSync(path.resolve(__dirname, '../views/templates/index.ejs'), {
-    encoding: 'utf-8'
-  });
-  const markdownList = fs.readdirSync(path.resolve(__dirname, '../content'), {
-    encoding: 'utf-8'
-  });
-  ctx.body = ejs.render(html,
-  {list: markdownList.map(item => item.replace(/.md/, ''))},
-  {filename: path.resolve(__dirname, '../views/templates/index.ejs')}
-  );
+  ctx.body = article.getList();
 });
 
 router.get('/:name', ctx => {
   const { name } = ctx.params;
-  const encoding = { encoding: 'utf-8' };
-  const content = fs.readFileSync(
-    path.resolve(__dirname, `../content/${name}.md`),
-    encoding
-  );
-  const md = new MarkdownIt();
-  const htmlStr = md.render(content);
-  const html = fs.readFileSync(
-    path.resolve(__dirname, '../views/templates/detail.ejs'),
-    encoding
-  );
-  ctx.body = ejs.render(html,
-      {content: htmlStr, title: name},
-      {filename: path.resolve(__dirname, '../views/templates/detail.ejs')}
-  );
+  ctx.body = article.getContent(name);
 })
 
 module.exports = router;
--- service/article.js
+++ service/article.js
@@ -1,7 +1,37 @@
-export function getList() {
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+const MarkdownIt = require('markdown-it');
 
+module.exports.getList = function() {
+  const indexPath = path.resolve(__dirname, '../views/templates/index.ejs');
+  const contentPath = path.resolve(__dirname, '../content');
+  const html = fs.readFileSync(indexPath, {
+    encoding: 'utf-8'
+  });
+  const markdownList = fs.readdirSync(contentPath, {
+    encoding: 'utf-8'
+  });
+  return ejs.render(html,
+  {list: markdownList.map(item => item.replace(/.md/, ''))},
+  {filename: indexPath}
+  );
 }
 
-export function getContent() {
-
+module.exports.getContent = function(name) {
+  const encoding = { encoding: 'utf-8' };
+  const content = fs.readFileSync(
+      path.resolve(__dirname, `../content/${name}.md`),
+      encoding
+  );
+  const md = new MarkdownIt();
+  const htmlStr = md.render(content);
+  const html = fs.readFileSync(
+      path.resolve(__dirname, '../views/templates/detail.ejs'),
+      encoding
+  );
+  return ejs.render(html,
+  {content: htmlStr, title: name},
+  {filename: path.resolve(__dirname, '../views/templates/detail.ejs')}
+  );
 }
